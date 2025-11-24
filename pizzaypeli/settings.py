48251369aeb4a365d4ac511baf485c2b2e97566f
Mar <mar@coconauts.net>
2025-11-24 11:52:50 +0100

    rename cartelera to showtimes

--- pizzaypeli/settings.py
+++ pizzaypeli/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'cartelera.apps.CarteleraConfig',
+    'showtimes.apps.ShowtimesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- showtimes/apps.py
+++ showtimes/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CarteleraConfig(AppConfig):
+class ShowtimesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cartelera'
+    name = 'showtimes'
